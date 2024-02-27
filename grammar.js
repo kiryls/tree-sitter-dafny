851e92f4e3d1a409bf5eb3b879a321feef0e1f65
kiryls <kiryl92@gmail.com>
2024-02-27 22:39:14 +0100

    grammar file refactoring + expressions

--- grammar.js
+++ grammar.js
@@ -12,68 +12,7 @@ module.exports = grammar({
       $.stringToken
     ),
 
-    // \b(?!abstract|allocated|as|assert|assume|bool|break|by|calc|case|char|class|codatatype|const|constructor|continue|datatype|decreases|else|ensures|exists|expect|export|extends|false|for|forall|fresh|function|ghost|if|imap|import|in|include|int|invariant|is|iset|iterator|label|lemma|map|match|method|modifies|modify|module|multiset|nameonly|nat|new|newtype|null|object|object?|old|opaque|opened|ORDINALpredicate|print|provides|reads|real|refines|requires|return|returns|reveal|reveals|seq|set|static|string|then|this|trait|true|twostate|type|unchanged|var|while|witness|yield|yields\b)\w+\b
-
-    Attribute: $ => seq('{:', $.AttributeName, optional($.Expressions), '}'),
-
-    AttributeName: $ => $.NoUSIdent,
-
-    NoUSIdent: $ => seq(
-      $.noUSNondigitIdChar,
-      repeat($.idchar)
-    ),
-
-    noUSNondigitIdChar: $ => /[a-zA-Z?']/,
-    noUSIdChar: $ => /[a-zA-Z0-9?']/,
-    letter: $ => /[a-zA-Z]/,
-    digit: $ => /[0-9]/,
-    special: $ => /[?_']/,
-    nondigitIdChar: $ => /[a-zA-Z?'_]/,
-    idchar: $ => /[a-zA-Z0-9?'_]/,
-
-    Ident: $ => $.ident,
-
-    digits: $ => seq($.digit, repeat(seq(optional('_'), $.digit))),
-
-    WildIdent: $ => choice($.NoUSIdent, '_'),
-    
-    // todo: exclude somehow charToken and reservedwords
-    ident: $ => seq(
-      $.nondigitIdChar,
-      repeat($.idchar)
-    ),
-
-
-    Expressions: $ => seq(
-      $.Expression,
-      repeat(seq(',', $.Expression))
-    ),
-
-    Expression: $ => token('[EXPR]'),
-
-
-    stringToken: $ => choice(
-      seq( '"', repeat(choice($.stringChar, $.escapedChar)), '"'),
-      seq( '@', '"', repeat(choice($.verbatimStringChar, seq('"', '"'))), '"'),
-    ),
-
-    stringChar: $ => /[^"\\\n\r]/,
-
-    escapedChar: $ => choice(
-      "\\'",
-      '\\"',
-      '\\\\',
-      '\\0',
-      '\\n',
-      '\\r',
-      '\\t',
-      seq('\\u', $.hexdigit, $.hexdigit, $.hexdigit, $.hexdigit),
-      seq('\\U{', repeat1($.hexdigit), '}'),
-    ),
-
-    verbatimStringChar: $ => /[^"]/,
-
-    hexdigit: $ => /[0-9a-fA-F]/,
+    DeclModifier: $ => choice('abstract', 'ghost', 'static', 'opaque'),
 
     TopDecl: $ => seq(
       repeat($.DeclModifier),
@@ -83,7 +22,10 @@ module.exports = grammar({
       )
     ),
 
-    DeclModifier: $ => choice('abstract', 'ghost', 'static', 'opaque'),
+
+    //
+    // Modules
+    //
 
     SubModuleDecl: $ => choice($.ModuleDefinition, $.ModuleImport, $.ModuleExport),
 
@@ -126,6 +68,85 @@ module.exports = grammar({
     ModuleName: $ => $.NoUSIdent,
 
     ExportId: $ => choice($.NoUSIdent, $.digits),
+
+
+    //
+    // Strings
+    //
+
+    stringToken: $ => choice(
+      seq( '"', repeat(choice($.stringChar, $.escapedChar)), '"'),
+      seq( '@', '"', repeat(choice($.verbatimStringChar, seq('"', '"'))), '"'),
+    ),
+
+    stringChar: $ => /[^"\\\n\r]/,
+
+    escapedChar: $ => choice(
+      "\\'",
+      '\\"',
+      '\\\\',
+      '\\0',
+      '\\n',
+      '\\r',
+      '\\t',
+      seq('\\u', $.hexdigit, $.hexdigit, $.hexdigit, $.hexdigit),
+      seq('\\U{', repeat1($.hexdigit), '}'),
+    ),
+
+    verbatimStringChar: $ => /[^"]/,
+
+
+
+
+    // \b(?!abstract|allocated|as|assert|assume|bool|break|by|calc|case|char|class|codatatype|const|constructor|continue|datatype|decreases|else|ensures|exists|expect|export|extends|false|for|forall|fresh|function|ghost|if|imap|import|in|include|int|invariant|is|iset|iterator|label|lemma|map|match|method|modifies|modify|module|multiset|nameonly|nat|new|newtype|null|object|object?|old|opaque|opened|ORDINALpredicate|print|provides|reads|real|refines|requires|return|returns|reveal|reveals|seq|set|static|string|then|this|trait|true|twostate|type|unchanged|var|while|witness|yield|yields\b)\w+\b
+
+    Attribute: $ => seq('{:', $.AttributeName, optional($.Expressions), '}'),
+
+    AttributeName: $ => $.NoUSIdent,
+
+    NoUSIdent: $ => seq(
+      $.noUSNondigitIdChar,
+      repeat($.idchar)
+    ),
+
+    noUSNondigitIdChar: $ => /[a-zA-Z?']/,
+    noUSIdChar: $ => /[a-zA-Z0-9?']/,
+    letter: $ => /[a-zA-Z]/,
+    digit: $ => /[0-9]/,
+    special: $ => /[?_']/,
+    nondigitIdChar: $ => /[a-zA-Z?'_]/,
+    idchar: $ => /[a-zA-Z0-9?'_]/,
+
+    digits: $ => seq($.digit, repeat(seq(optional('_'), $.digit))),
+
+    hexdigit: $ => /[0-9a-fA-F]/,
+
+    Ident: $ => $.ident,
+
+    WildIdent: $ => choice($.NoUSIdent, '_'),
+    
+    // todo: exclude somehow charToken and reservedwords
+    ident: $ => seq(
+      $.nondigitIdChar,
+      repeat($.idchar)
+    ),
+
+
+    //
+    // Expressions
+    //
+
+    Expressions: $ => seq(
+      $.Expression,
+      repeat(seq(',', $.Expression))
+    ),
+
+    Expression: $ => seq($.EquivExpression, optional(seq(';', $.Expression))),
+
+    EquivExpression: $ => seq($.ImpliesExpliesExpression, repeat(seq('<==>', $.ImpliesExpliesExpression))),
+
+    ImpliesExpliesExpression: $ => '#',
+
 
   }
 });
